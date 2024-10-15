01f7bbc1331b811fbb31165a82e201283e7008fe
azuraservices <stanislao97s@gmail.com>
2024-10-15 12:20:40 +0200

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#FFFFFF" />
       </head>
       <body className={inter.className}>
         {children}
